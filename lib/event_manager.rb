43ccf6218cbb48c709544caada5b7365f32745dd
Andrew Dymond <andy.dymond@gmail.com>
2017-12-28 18:57:32 -0700

    Update event_manager and form_letter to support erb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,20 @@
 require "csv"
 require "google/apis/civicinfo_v2"
+require "erb"
 
 class EventManager
 
-  attr_reader :contents, :civic_info, :template_letter
+  attr_reader :contents,
+              :civic_info,
+              :template_letter,
+              :erb_template
 
   def initialize(csv_file = "./data/event_attendees.csv")
     @contents = CSV.open csv_file, headers: true, header_converters: :symbol
     @civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
     @civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-    @template_letter = File.read "./lib/form_letter.html"
+    @template_letter = File.read "./lib/form_letter.erb"
+    @erb_template = ERB.new(template_letter)
   end
 
   def start_message
@@ -18,14 +23,24 @@ class EventManager
 
   def parse_csv_file
     contents.each do |row|
+      id = row[0]
       name = row[:first_name]
       zipcode = clean_zipcode(row[:zipcode])
-      legislator_names = legislator_names_by_zipcode(zipcode)
+      legislators = legislators_by_zipcode(zipcode)
 
-      personal_letter = template_letter.gsub("FIRST_NAME", name)
-      personal_letter.gsub!("LEGISLATORS", legislator_names)
+      form_letter = erb_template.result(binding)
 
-      puts personal_letter
+      save_thank_you_letters(id, form_letter)
+    end
+  end
+
+  def save_thank_you_letters(id, form_letter)
+    Dir.mkdir("./lib/output") unless Dir.exists?("./lib/output")
+
+    filename = "output/thanks_#{id}"
+
+    File.open(filename, "w") do |file|
+      puts form_letter
     end
   end
 
@@ -33,7 +48,7 @@ class EventManager
     zipcode.to_s.rjust(5, "0")[0..4]
   end
 
-  def legislator_names_by_zipcode(zipcode)
+  def legislators_by_zipcode(zipcode)
     begin
     legislators = civic_info.representative_info_by_address(
       address: zipcode,
@@ -41,7 +56,6 @@ class EventManager
       roles: ['legislatorUpperBody', 'legislatorLowerBody'])
 
       legislators = legislators.officials
-      legislators.map(&:name).join(", ")
     rescue
       "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
     end
